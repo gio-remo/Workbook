c5b31d4a4007a10f891cb26f67d7444a19566d12
gio-va <giogoesgreen@gmail.com>
2022-01-01 11:52:33 +0100

    New lessons and ex

---
+++ Py4e-Scientific-Computing-with-Python/8 _Dictionaries.py
@@ -0,0 +1,47 @@
+#####
+# L16 - Python Dictionaries
+#####
+
+bag = dict()
+
+bag['a'] = 12
+bag['b'] = 8
+
+print(bag)
+print(bag['a'])
+
+other = { }
+print(other)
+
+
+#####
+# L17 - Dictionaries: Common Applications
+#####
+
+d = dict()
+print('key' in d) # False
+
+counts = dict()
+names = ['al', 'ol', 'al', 'il', 'il']
+for n in names :
+    if n not in counts :
+        counts[n] = 1
+    else :
+        counts[n] += 1
+
+
+print(counts)
+print(counts.get('al')) # 2
+
+
+#####
+# L18 - Dictionaries and Loops
+#####
+
+bag = dict()
+
+bag['a'] = 12
+bag['b'] = 8
+
+print(bag.keys())
+print(bag.values())
